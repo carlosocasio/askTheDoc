3e06e279809fe14358cc146a05d4bf9aa18eb97b
Carlos Ocasio <carlos@ocasio.me>
2025-05-03 20:49:15 -0400

    Update askDoc.py

--- askDoc.py
+++ askDoc.py
@@ -18,14 +18,14 @@ def generate_response(uploaded_file, openai_api_key, query_text):
     if uploaded_file is not None:
         documents = [uploaded_file.read().decode()]
         # Split documents into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         texts = text_splitter.create_documents(documents)
         # Select embeddings
         embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
         # Create a vectorstore from documents
         db = Chroma.from_documents(texts, embeddings)
         # Create retriever interface
-        retriever = db.as_retriever()
+        retriever = db.as_retriever(search_kwargs={"k": 3})
         # Create QA chain
         qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
         return qa.run(query_text)
